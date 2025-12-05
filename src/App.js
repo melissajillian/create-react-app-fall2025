a0f2e4d4e926d6a61e4e0a69129ed8104398b69a
Melissa Jillian Walters Collins <mjwalt@umich.edu>
2025-12-05 19:54:33 +0000

    push 2

--- src/App.js
+++ src/App.js
@@ -20,15 +20,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Comp />
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
         <button
           onClick={() => setCount(count + 1)}
         >Click Me</button>
